6bfccbf219630226b91a9c007d6fd45cc9251c9b
wanghuan <18501667737@163.com>
2022-04-18 17:07:04 +0800

    初始化

---
+++ src/main/java/com/uwjx/push/UwjxPushServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwjxPushServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UwjxPushServiceApplication.class, args);
+    }
+
+}
